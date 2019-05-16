a40d0a6c4a43cccdb6ce4e031216401ec921a5e1
Aleaxndre Paixao <aspaixao@gmail.com>
2019-05-16 04:09:18 +0100

    I changed the type.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hello, my name's Alexandre Paixao.");
+        System.out.println("Hi, my name's Alexandre Paixao.");
     }
 
 }
