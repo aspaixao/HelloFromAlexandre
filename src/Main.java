e26a1e8020771709a6a0be1c087a3475beda9448
Aleaxndre Paixao <aspaixao@gmail.com>
2019-05-16 03:42:05 +0100

    This is my Hello.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello, my name's Alexandre Paixao.");
+    }
+
+}
