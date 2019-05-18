a28451fd539c444851c8145bf18ff9b2a5f13891
Alexandre Paixao <aspaixao@gmail.com>
2019-05-18 11:50:15 +0100

    Update Main.java
    Change comments.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("╔═════════════════════════════════════════════════╗"); //187
-        System.out.println("║                    SAY HELLO                    ║"); //187
-        System.out.println("╚═════════════════════════════════════════════════╝"); //187
+        System.out.println("╔═════════════════════════════════════════════════╗");
+        System.out.println("║                    SAY HELLO                    ║");
+        System.out.println("╚═════════════════════════════════════════════════╝");
         System.out.println("\t> Hi, my name's Alexandre Paixao.");
         System.out.println("\n\t> Hello Alexandre Paixao, I am Margareth!");
     }
